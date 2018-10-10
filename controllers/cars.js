8f0e79f7c06f9b5620aae46a7f8b949f89f29962
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-10 12:06:29 -0500

    commit just cause we can

--- controllers/cars.js
+++ controllers/cars.js
@@ -9,6 +9,21 @@ router.get('/', (req, res)=>{
 	})
 })
 
+router.get('/new', (req, res)=>{
+	res.render('new.ejs');
+})
+
+router.get('/:index', (req, res)=>{
+	res.render('show.ejs', {
+		cars: Cars[req.params.index],
+	})
+})
+
+router.post('/', (req, res)=>{
+	Cars.push(req.body);
+	res.redirect('/cars')
+})
+
 router.delete('/:id', (req, res) => {
   console.log(req.params.id, ' id in delete route');
   Cars.splice(req.params.id, 1);
